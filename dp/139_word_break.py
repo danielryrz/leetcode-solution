23ca1f8f5ad3b87dfa631c7303dd697a56f3dd3a
danielryrz <119617824+danielryrz@users.noreply.github.com>
2025-10-26 10:56:46 +0000

    Refactor wordBreak for clarity and consistency
    Refactor wordBreak function to improve readability and variable naming.

--- dp/139_word_break.py
+++ dp/139_word_break.py
@@ -1,12 +1,21 @@
-def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-  wordSet = set(wordDict)
-  dp = [False] * (len(s) + 1)
-  dp[0] = True
+def wordBreak(s, wordDict):
+    word_set = set(wordDict)
+
+    # dp[i] means "can we segment s[:i] ?"  (i from 0 to len(s))
+    dp = [False] * (len(s) + 1)
   
-  for i in range(1, len(s)+1):
-    for w in wordSet:
-        if len(w) <= i and s[i-len(w):i] == w and dp[i-len(w)] :
-            dp[i] = True
-            break
-  
-  return dp[-1]
+    # empty prefix is segmentable
+    dp[0] = True  
+
+    # check prefix lengths 1..len(s)+1
+    for i in range(1, len(s) + 1):            
+        for w in word_set:
+
+            # word fits into prefix of length i               
+            if len(w) <= i: 
+                                
+                # check the prefix BEFORE this word is good
+                if dp[i - len(w)] and s[i - len(w):i] == w:
+                    dp[i] = True         # s[:i] is segmentable
+                    break                # no need to try other words for this i -> move to the next word
+    return dp[-1] #check the last dp value: either True or False
