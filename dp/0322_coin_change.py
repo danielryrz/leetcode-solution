14666d1417bcd28bf7e844649fc7d17ea1c8a2b8
danielryrz <119617824+danielryrz@users.noreply.github.com>
2025-11-02 11:49:10 +0000

    Improve comments for clarity in coinChange function
    Clarify comments. Have another go through the problem to refresh the knowledge.

--- dp/0322_coin_change.py
+++ dp/0322_coin_change.py
@@ -25,17 +25,17 @@ class Solution:
         
         dp = [float('inf')] * (amount + 1)
 
-        # It takes 0 coins to make up amount 0
+        # It takes 0 coins to make up to 0
         dp[0] = 0
 
         # Build dp from 1 up to the target amount
         for i in range(1, amount + 1):
             # Iterate through every coin to find the min number of coins to make amount i
             for coin in coins:
-                # The coin can only be used if it does not exceed the current amount
+                # The coin can only be used if it is less than or equal to i
                 if i - coin >= 0:
-                    # Take the minimum number of coins to make up value i
+                    # Take the minimum number of coins to make up to value i
                     dp[i] = min(dp[i], dp[i - coin] + 1)
 
-        # Return result; if dp[-1] is still infinity, amount cannot be made
+        # Return result; if dp[-1] is still infinity, it means the amount cannot be made and return -1
         return dp[-1] if dp[-1] != float('inf') else -1
